e412b95b49dcb68b18c30a577b92b9f330e655d7
kjh3165 <jhwk109@gmail.com>
2026-03-19 04:12:12 +0900

    todoapp-4

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,6 +1,6 @@
 function TodoItem({ todo, removeTodo, toggleTodo }) {
     return (
-        <li>
+        <li style={{ textDecorationLine: todo.checked ? 'line-through' : 'none' }}>
             <input type="checkbox" onChange={() => toggleTodo(todo.id)} checked={todo.checked} />
             {JSON.stringify(todo.checked)} | {todo.id} : {todo.text}
             <button onClick={() => removeTodo(todo.id)}>X</button>
