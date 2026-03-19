7418113a80f791f24a049a70b0b0b890b26b7ea4
kjh3165 <jhwk109@gmail.com>
2026-03-19 10:51:13 +0900

    todoapp-5

--- src/useTodos.js
+++ src/useTodos.js
@@ -1,38 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getItem, setItem } from './storage'
 
 function useTodos() {
-    const [todos, setTodos] = useState([])
-    const [lastId, setLastId] = useState(1)
+    const [todos, setTodos] = useState(() => {
+        const storedTodos = getItem('todos', [])
+        return storedTodos
+    })
+    const [lastId, setLastId] = useState(() => {
+        const storedLastId = getItem('lastId', 1)
+        return storedLastId
+    })
+
+    useEffect(() => {
+        setItem('todos', todos)
+        setItem('lastId', lastId)
+    }, [todos, lastId])
 
     const addTodo = (text) => {
-        const todos = getItem('todos', [])
-        const lastId = getItem('lastId', 1)
         setTodos([...todos, { id: lastId, text: text, checked: false }])
         setLastId(lastId + 1)
-        setItem('todos', [...todos, { id: lastId, text: text, checked: false }])
-        setItem('lastId', lastId + 1)
     }
 
     const removeTodo = (id) => {
-        const todos = getItem('todos', [])
         setTodos(todos.filter((todo) => todo.id != id))
-        setItem(
-            'todos',
-            todos.filter((todo) => todo.id != id),
-        )
     }
 
     const toggleTodo = (id) => {
-        const todos = getItem('todos', [])
         setTodos(todos.map((todo) => (todo.id != id ? todo : { ...todo, checked: !todo.checked })))
-        setItem(
-            'todos',
-            todos.map((todo) => (todo.id != id ? todo : { ...todo, checked: !todo.checked })),
-        )
     }
 
     return {
+        todos,
         addTodo,
         removeTodo,
         toggleTodo,
